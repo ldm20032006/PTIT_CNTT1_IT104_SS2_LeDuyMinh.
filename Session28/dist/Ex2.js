9aa1accce8d139d12e63be7fe9c505a7aafb426e
ldm20032006 <leduyminh2006k4@gmail.com>
2025-09-14 22:14:29 +0700

    Add files via upload

---
+++ Session28/dist/Ex2.js
@@ -0,0 +1,8 @@
+"use strict";
+// type Callback = () => void;
+// function delayedCallback(callback: Callback, delay: number) {
+//   setTimeout(() => {callback()}, delay);
+// }
+// delayedCallback(() => {
+//   console.log("Delay 3s");
+// }, 3000);
